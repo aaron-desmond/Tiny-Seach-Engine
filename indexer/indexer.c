5b3721624463c683c0f11ba56f68571b26ecf0f5
charlottecrawford363 <charlotte.g.crawford.29@dartmouth.edu>
2026-05-27 19:01:12 -0400

    add indexer files

---
+++ indexer/indexer.c
@@ -0,0 +1,10 @@
+
+
+/* Function Prototypes */
+index_t* indexBuild(index_t* index, char* pageDirectory);
+void indexPage(index_t* index, webpage_t* webpage, int docID);
+
+void main(const int argc, char* argv[]) {
+
+}
+
