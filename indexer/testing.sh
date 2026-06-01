8fda4ec637359b701579cad69ff18b8ca36c720b
Lila Hwang <lila.m.hwang.28@dartmouth.edu>
2026-06-01 16:22:50 -0400

    Current testing.sh file

---
+++ indexer/testing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# testing.sh - test script for indexer
+
+# Lila Hwang, May 2026
+
+echo "********** Testing invalid arguments **********"
+./indexer
+
+echo "********** Testing invalid directory **********"
+./indexer fooDir test-index
+
+echo "********* Testing valid crawl directory ***********"
+./indexer ../crawler/data/letters-10 test-index
+
+echo "********** Testing indextest ************"
+./indextest test-index test-index-2
+
+echo "********* Comparing index files **********"
+sort test-index > sorted1
+sort test-index-2 > sorted2
+
+diff sorted1 sorted2
+
+rm -f sorted1 sorted2
+
+echo "********* Running Valgrind ***********"
+valgrind --leak-check=full ./indexer ../crawler/data/letters-10 test-index
+
+echo "********** Done **********"
